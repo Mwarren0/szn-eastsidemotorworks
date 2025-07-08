52867e6fad122203568776f379334aef945b4528
Mike Warren <162091481+Mwarren0@users.noreply.github.com>
2025-07-08 15:17:44 -0500

    Major update

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function generateInvoice() {
   document.querySelector("#invoices tbody").appendChild(row);
 
   // Send to Discord
-  fetch("https://discord.com/api/webhooks/TU_WEBHOOK_URL", {
+  fetch("https://discord.com/api/webhooks/1392217195953786962/rn3McMeacAiTIzjN1sNmTxF8iPYW3QDNlRpsaUFMoH61SmJl59fPgO4sjX6lQEvxzJAD", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
